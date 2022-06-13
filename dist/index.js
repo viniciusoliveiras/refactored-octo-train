fc856a7e12662f8f8f3e2cbf3d5e17535196636d
viniciusoliveiras <vinitag190@gmail.com>
2022-06-13 16:37:30 -0300

    package name

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var ExampleComponent = function ExampleComponent(_ref) {
+  var text = _ref.text;
+  return React.createElement("div", {
+    className: 'bg-red-300 text-black p-4'
+  }, "Example Component: ", text);
+};
+
+exports.ExampleComponent = ExampleComponent;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.modern.js
@@ -0,0 +1,11 @@
+import { createElement } from 'react';
+
+var ExampleComponent = function ExampleComponent(_ref) {
+  var text = _ref.text;
+  return createElement("div", {
+    className: 'bg-red-300 text-black p-4'
+  }, "Example Component: ", text);
+};
+
+export { ExampleComponent };
+//# sourceMappingURL=index.modern.js.map
